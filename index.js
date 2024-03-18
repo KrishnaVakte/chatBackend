30fcff785fa6fe7ecc348553d15baefc7ca2635e
KrishnaVakte <86585840+KrishnaVakte@users.noreply.github.com>
2024-03-19 00:06:26 +0530

    cors

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const app = express();
 const socket = require("socket.io")
 require("dotenv").config();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://chat-front-khaki.vercel.app'
+}));
 app.use(express.json());
 
 app.use("/api", userroute);
