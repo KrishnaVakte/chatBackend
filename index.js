0a2ff82df71ee308aea3da9ca64ea8017f953742
KrishnaVakte <86585840+KrishnaVakte@users.noreply.github.com>
2024-03-19 13:33:46 +0530

    cors solver

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.use(cors({
   origin: '*',
 }));
 app.use(express.json());
+pp.use((req, res, next) => {
+  //allow access from every, elminate CORS
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.removeHeader('x-powered-by');
+  //set the allowed HTTP methods to be requested
+  res.setHeader('Access-Control-Allow-Methods','POST');
+  //headers clients can use in their requests
+  res.setHeader('Access-Control-Allow-Headers','Content-Type');
+  //allow request to continue and be handled by routes
+  next();
+});
 
 app.use("/api", userroute);
 app.use("/api", messageroute);
